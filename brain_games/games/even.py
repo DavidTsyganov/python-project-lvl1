2fe5472d677be9a594c77505a37a6a82594e9733
Ganesha_here <Jamper1488@gmail.com>
2020-04-23 14:17:14 +0300

    another refactoring lap

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,7 +5,7 @@ describer = 'Answer "yes" if number even otherwise answer "no".'
 def is_even(number):
     return not number % 2
 
-def logic_game():
+def run_game():
     quest = randint(1,99)
     result = 'yes' if is_even(quest) else 'no'
     expression = ' {}'.format(quest)
