f93a4981d2a194807c60287ab141d60af041eec8
Ganeshahere <jamper1488@gmail.com>
2020-05-17 12:58:52 +0300

    ref engine ch1

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,11 +4,14 @@ ROUNDS = 3
 
 
 def run(name_game):
-    wrong_answer = """'{0}' is wrong answer ;(. Correct answer was '{1}'. Let's try again, {2}!"""
+    wrong_answer = """'{0}' is wrong answer ;(. Correct answer was '{1}'.
+    Let's try again, {2}!"""
     print("Welcome to the Brain Games!")
     print()
     name = prompt.string('May I have your name?')
-    while name == 'yes' or name == 'no' or name == 'No' or name == 'Yes' or name == 'NO' or name == 'nO' or name == 'yES' or name == 'yeS' or name == 'YeS':
+    while name == 'yes' or name == 'no' or name == 'No' or name == 'Yes' or 
+    name == 'NO' or name == 'nO' or name == 'yES' or name == 'yeS' or 
+    name == 'YeS':
         name = prompt.string('Try to input your name correctly')
     print('Hello, {}!'.format(name))
     print()
