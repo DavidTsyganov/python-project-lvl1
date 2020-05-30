a565a71a15e28213af00870931550c7b91eb5fa4
Ganeshahere <jamper1488@gmail.com>
2020-05-30 15:29:39 +0300

    new quotes, engine with techical requirements.

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,23 +1,19 @@
 import prompt
 
-ROUNDS = 3
+ROUND = 3
 
 
-def run(name_game):
+def run(game):
     wrong_answer = """'{0}' is wrong answer ;(. Correct answer was '{1}'.\n
     Let's try again, {2}!"""
     print("Welcome to the Brain Games!")
     print()
     name = prompt.string('May I have your name?')
-    while name == 'yes' or name == 'no' or name == 'No' or name == 'Yes' \
-            or name == 'NO' or name == 'nO' or name == 'yES' or name == 'yeS' \
-            or name == 'YeS':
-        name = prompt.string('Try to input your name correctly')
     print('Hello, {}!'.format(name))
     print()
-    print(name_game.DESCRIBER)
-    for _try_num in range(0, ROUNDS):
-        expression, result = name_game.run_game()
+    print(game.DESCRIBER)
+    for _try_num in range(0, ROUND):
+        expression, result = game.run_game()
         print('Question:', expression)
         answer = prompt.string('Your answer: ')
         if answer != result:
--- brain_games/games/test.py
+++
@@ -1,7 +0,0 @@
-from prime import is_prime
-
-
-print(is_prime(0))
-print(is_prime(-1))
-print(is_prime(7))
-print(is_prime(9))
