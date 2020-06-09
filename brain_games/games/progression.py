51a2ec64577bd6d4f89e5d1c355302f1c10cc056
Ganeshahere <jamper1488@gmail.com>
2020-06-09 19:19:50 +0300

    fixed progression and naming

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,23 +1,16 @@
-from random import randint
+from random import randint, choice
 
 DESCRIBER = 'What number is missing in the progression?'
 PROGRESSION_LENGTH = 10
 
 
 def make_data_for_game():
-    number1 = randint(1, 99)
+    start_number = randint(1, 99)
     step = randint(1, 99)
-    counter = 0
-    question = ''
-    hidden_number = randint(0, PROGRESSION_LENGTH - 1)
-    while counter < PROGRESSION_LENGTH:
-        current_number = number1 + (counter * step)
-        if counter == hidden_number:
-            element = '.. '
-            result = str(current_number)
-        else:
-            element = str(current_number) + " "
-        question += element
-        counter += 1
-    expression = str(question)
+    maximum_number = (step * PROGRESSION_LENGTH) + start_number
+    progression_index = range(start_number, maximum_number, step)
+    result = choice(progression_index)
+    progression = ' '.join([
+'..' if num == result else str(num) for num in progression_index])
+    expression = f'{progression}'
     return expression, result
